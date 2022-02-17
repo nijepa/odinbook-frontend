d1a92f189b33675a13ca89ba1b6ed41d2524ed18
Nikola Pavicevic <nijepa@gmail.com>
2022-02-17 22:04:46 +0100

    clearing store

--- src/store/local_storage.js
+++ src/store/local_storage.js
@@ -1,24 +1,19 @@
 function saveToken(token) {
-	localStorage.setItem('odinbook_user', JSON.stringify(token));
+  localStorage.setItem("odinbook_user", JSON.stringify(token));
 }
 
 function getToken() {
-	return JSON.parse(localStorage.getItem('odinbook_user'));
+  return JSON.parse(localStorage.getItem("odinbook_user"));
 }
 
 function removeToken() {
-	localStorage.removeItem('odinbook_user');
+  localStorage.removeItem("odinbook_user");
 }
 
 function updateToken(data) {
-	const savedCred = JSON.parse(localStorage.getItem('odinbook_user'));
-	savedCred.user = data;
-	localStorage.setItem('odinbook_user', JSON.stringify(savedCred));
+  const savedCred = JSON.parse(localStorage.getItem("odinbook_user"));
+  savedCred.user = data;
+  localStorage.setItem("odinbook_user", JSON.stringify(savedCred));
 }
 
-export {
-	saveToken,
-	getToken,
-	removeToken,
-	updateToken
-}
+export { saveToken, getToken, removeToken, updateToken };
--- src/store/post.js
+++ src/store/post.js
@@ -1,12 +1,12 @@
-import Vue from 'vue';
-import Vuex from 'vuex';
-import axios from 'axios';
+import Vue from "vue";
+import Vuex from "vuex";
+import axios from "axios";
 //const URL = process.env.VUE_APP_BACKEND_URL_LOCAL;
 const URL = process.env.VUE_APP_BACKEND_URL;
-import apiClient from './api_client';
-import router from '../router';
+import apiClient from "./api_client";
+import router from "../router";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 const state = {
   allPosts: [],
@@ -20,18 +20,17 @@ const state = {
 
 /* -------------------------------------- GETTERS -------------------------------------- */
 const getters = {
-  getAllPosts: state => state.posts,
-  getIsTimeline: state => state.isTimeline,
-  getUserPosts: state => state.userPosts,
-  getPost: state => state.post,
-  allComments: state => state.comments,
-  getSelectedPost: state => state.selectedPost,
-  getFavPosts: state => state.allPosts
+  getAllPosts: (state) => state.posts,
+  getIsTimeline: (state) => state.isTimeline,
+  getUserPosts: (state) => state.userPosts,
+  getPost: (state) => state.post,
+  allComments: (state) => state.comments,
+  getSelectedPost: (state) => state.selectedPost,
+  getFavPosts: (state) => state.allPosts,
 };
 
 /* -------------------------------------- MUTATIONS -------------------------------------- */
 const mutations = {
-
   setAllPosts(state, posts) {
     state.allPosts = posts;
   },
@@ -49,7 +48,7 @@ const mutations = {
   },
 
   setisTimeline(state, isTimeline) {
-    state.isTimeline = isTimeline
+    state.isTimeline = isTimeline;
   },
 
   updateUserPosts(state, userPosts) {
@@ -69,16 +68,16 @@ const mutations = {
   },
 
   addPost(state, text) {
-    state.posts.posts = [text, ...state.posts.posts]
+    state.posts.posts = [text, ...state.posts.posts];
   },
-  
+
   updatePost(state, post) {
     state.posts.posts = [
-      ...state.posts.posts.map(item => 
-          item._id !== post._id ? item : {...item, ...post}
-      )
-    ] 
-/*     state.posts = state.posts.map(post => {
+      ...state.posts.posts.map((item) =>
+        item._id !== post._id ? item : { ...item, ...post }
+      ),
+    ];
+    /*     state.posts = state.posts.map(post => {
       if (post.id === post.id) {
         return Object.assign({}, post, data)
       }
@@ -86,194 +85,224 @@ const mutations = {
     }) */
   },
 
-  deletePost (state, id) {
+  deletePost(state, id) {
     state.posts.posts = [
-      ...state.posts.posts.filter((item) => item._id !== id)
+      ...state.posts.posts.filter((item) => item._id !== id),
     ];
   },
 
-  addComment (state, comment) {
-    const objIndex = state.posts.posts.map(function(x) {return x._id; }).indexOf(comment._id);
+  addComment(state, comment) {
+    const objIndex = state.posts.posts
+      .map(function (x) {
+        return x._id;
+      })
+      .indexOf(comment._id);
     Object.assign(state.posts.posts[objIndex], { comments: comment.comments });
   },
 
-  deleteComment (state, id) {
-    const objIndex = state.posts.posts.map(function(x) {return x._id; }).indexOf(id.postId);
+  deleteComment(state, id) {
+    const objIndex = state.posts.posts
+      .map(function (x) {
+        return x._id;
+      })
+      .indexOf(id.postId);
     state.posts.posts[objIndex].comments = [
-      ...state.posts.posts[objIndex].comments.filter((item) => item._id !== id.commentId)
+      ...state.posts.posts[objIndex].comments.filter(
+        (item) => item._id !== id.commentId
+      ),
     ];
   },
 
-  updateLike (state, like) {
-    const objIndex = state.posts.posts.map(function(x) {return x._id; }).indexOf(like._id);
+  updateLike(state, like) {
+    const objIndex = state.posts.posts
+      .map(function (x) {
+        return x._id;
+      })
+      .indexOf(like._id);
     Object.assign(state.posts.posts[objIndex], { likes: like.likes });
   },
 
-  updateCommentLike (state, like) {
-    const objIndex = state.posts.posts.map(function(x) {return x._id; }).indexOf(like.postId);
-    const comIndex = state.posts.posts[objIndex].comments.map(function(x) {return x._id; }).indexOf(like._id);
-    Object.assign(state.posts.posts[objIndex].comments[comIndex], { likes: like.likes });
+  updateCommentLike(state, like) {
+    const objIndex = state.posts.posts
+      .map(function (x) {
+        return x._id;
+      })
+      .indexOf(like.postId);
+    const comIndex = state.posts.posts[objIndex].comments
+      .map(function (x) {
+        return x._id;
+      })
+      .indexOf(like._id);
+    Object.assign(state.posts.posts[objIndex].comments[comIndex], {
+      likes: like.likes,
+    });
   },
 };
 
 /* -------------------------------------- ACTIONS -------------------------------------- */
 const actions = {
-
   async loadAllPosts({ commit }) {
-    await axios.get(URL + 'posts').then((response) => {
-      commit('setAllPosts', response.data);
-      commit('changeLoadingState', false);
-    })
+    await axios.get(URL + "posts").then((response) => {
+      commit("setAllPosts", response.data);
+      commit("changeLoadingState", false);
+    });
   },
-  
+
   async loadPosts({ commit }, page) {
-    await axios.get(URL + 'posts/' + page).then((response) => {
-      commit('updatePosts', response.data);
-      commit('changeLoadingState', false);
-    })
+    await axios.get(URL + "posts/" + page).then((response) => {
+      commit("updatePosts", response.data);
+      commit("changeLoadingState", false);
+    });
   },
 
   async loadPost({ commit }, id) {
-    await axios.get(URL + 'posts/post/' + id).then((response) => {
-      commit('setPost', response.data);
-      const arr = {posts: []}
-      arr.posts.push(response.data)
-      commit('updatePosts', arr);
-      commit('clearUserPosts')
-      commit('changeLoadingState', false);
-    })
+    await axios.get(URL + "posts/post/" + id).then((response) => {
+      commit("setPost", response.data);
+      const arr = { posts: [] };
+      arr.posts.push(response.data);
+      commit("updatePosts", arr);
+      commit("clearUserPosts");
+      commit("changeLoadingState", false);
+    });
   },
 
   async syncPosts({ commit }, posts) {
-    commit('reupdatePosts', posts);
-      //commit('changeLoadingState', false);
+    commit("reupdatePosts", posts);
+    //commit('changeLoadingState', false);
   },
 
-  async choseTimeline( { commit }, chose) {
-    commit('setIsTimeline', chose);
+  async choseTimeline({ commit }, chose) {
+    commit("setIsTimeline", chose);
   },
 
   async loadUserPosts({ commit }, id) {
-    await axios.get(URL + 'posts/user/' + id).then((response) => {
-      commit('updateUserPosts', response.data);
-    })
+    await axios.get(URL + "posts/user/" + id).then((response) => {
+      commit("updateUserPosts", response.data);
+    });
   },
 
   async loadUserLikes({ commit }, id) {
-    await axios.get(URL + 'posts/posts/' + id).then((response) => {
-      commit('updateUserLikes', response.data);
-    })
+    await axios.get(URL + "posts/posts/" + id).then((response) => {
+      commit("updateUserLikes", response.data);
+    });
   },
 
   async loadComments({ commit }) {
-    await axios.get(URL + 'comments').then((response) => {
-      commit('updateComments', response.data)
-      commit('changeLoadingState', false)
-    })
+    await axios.get(URL + "comments").then((response) => {
+      commit("updateComments", response.data);
+      commit("changeLoadingState", false);
+    });
   },
 
   async createPost({ commit }, postData) {
-    await apiClient.post(URL + 'posts', postData)
+    await apiClient
+      .post(URL + "posts", postData)
       .then((response) => {
-        commit('addPost', response.data);
+        commit("addPost", response.data);
       })
       .catch((error) => {
         if (error.response) {
-          commit('setErrors', error.response.data.error);
+          commit("setErrors", error.response.data.error);
         } else {
-          commit('setErrors', error);
+          commit("setErrors", error);
         }
-      })
+      });
   },
 
   async createComment({ commit }, commentData) {
-    await apiClient.post(URL + 'posts/comment', commentData)
+    await apiClient
+      .post(URL + "posts/comment", commentData)
       .then((response) => {
-        commit('addComment', response.data);
+        commit("addComment", response.data);
       })
       .catch((error) => {
         if (error.response) {
-          commit('setErrors', error.response.data.error);
+          commit("setErrors", error.response.data.error);
         } else {
-          commit('setErrors', error);
+          commit("setErrors", error);
         }
-      })
+      });
   },
 
   async commentDelete({ commit }, postData) {
-    await apiClient.put(URL + 'posts/comment/' + postData[0].comment._id, postData)
+    await apiClient
+      .put(URL + "posts/comment/" + postData[0].comment._id, postData)
       .then((response) => {
-        commit('deleteComment', response.data, postData)
+        commit("deleteComment", response.data, postData);
       })
       .catch((error) => {
         if (error.response) {
-          commit('setErrors', error.response.data.error);
+          commit("setErrors", error.response.data.error);
         } else {
-          commit('setErrors', error);
+          commit("setErrors", error);
         }
-      })
+      });
   },
 
   postType({ commit }, post) {
-    commit('setSelectedPost', post);
+    commit("setSelectedPost", post);
     router.push("/post-add");
   },
 
   async postUpdate({ commit }, postData) {
-    await apiClient.put(URL + 'posts/' + postData._id, postData)
+    await apiClient
+      .put(URL + "posts/" + postData._id, postData)
       .then((response) => {
-        commit('updatePost', response.data);
+        commit("updatePost", response.data);
       })
       .catch((error) => {
         if (error.response) {
-          commit('setErrors', error.response.data.error);
+          commit("setErrors", error.response.data.error);
         } else {
-          commit('setErrors', error);
+          commit("setErrors", error);
         }
-      })
+      });
   },
 
   async postDelete({ commit }, postData) {
-    await apiClient.delete(URL + 'posts/' + postData._id, postData)
+    await apiClient
+      .delete(URL + "posts/" + postData._id, postData)
       .then((response) => {
-        commit('deletePost', response.data._id)
+        commit("deletePost", response.data._id);
       })
       .catch((error) => {
         if (error.response) {
-          commit('setErrors', error.response.data.error);
+          commit("setErrors", error.response.data.error);
         } else {
-          commit('setErrors', error);
+          commit("setErrors", error);
         }
-      })
+      });
   },
 
   async likeUpdate({ commit }, likeData) {
-    await apiClient.post(URL + 'posts/like/' + likeData.id, likeData)
+    await apiClient
+      .post(URL + "posts/like/" + likeData.id, likeData)
       .then((response) => {
-        commit('updateLike', response.data);
+        commit("updateLike", response.data);
       })
       .catch((error) => {
         if (error.response) {
-          commit('setErrors', error.response.data.error);
+          commit("setErrors", error.response.data.error);
         } else {
-          commit('setErrors', error);
+          commit("setErrors", error);
         }
-      })
+      });
   },
 
   async likeCommentUpdate({ commit }, likeData) {
-    await apiClient.post(URL + 'posts/comment/like/' + likeData.id, likeData)
+    await apiClient
+      .post(URL + "posts/comment/like/" + likeData.id, likeData)
       .then((response) => {
         response.data["postId"] = likeData.postId;
-        commit('updateCommentLike', response.data);
+        commit("updateCommentLike", response.data);
       })
       .catch((error) => {
         if (error.response) {
-          commit('setErrors', error.response.data.error);
+          commit("setErrors", error.response.data.error);
         } else {
-          commit('setErrors', error);
+          commit("setErrors", error);
         }
-      })
+      });
   },
 };
 
@@ -281,5 +310,5 @@ export default {
   state,
   getters,
   actions,
-  mutations
+  mutations,
 };
